7f39bf82d796c3ded5cacfcd98f3c8acdf14c0f5
Farlado <stultasergalo@gmail.com>
2019-09-18 12:26:01 -0400

    Remove laggy org-bullets

--- init.el
+++ init.el
@@ -26,7 +26,7 @@
  '(custom-safe-themes
    (quote
     ("bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476" default)))
- '(package-selected-packages (quote (spacemacs-theme which-key use-package org-bullets))))
+ '(package-selected-packages (quote (spacemacs-theme which-key use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
