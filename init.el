0b00b73123f2ee4d5b1aae911dbf514073f0459b
Farlado <stultasergalo@gmail.com>
2019-09-22 21:56:03 -0400

    Return init.el to smallfont, fix a calc issue

--- init.el
+++ init.el
@@ -36,4 +36,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 220 :width normal :family "Input Mono Compressed")))))
+ '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 100 :width normal :family "Input Mono Compressed")))))
