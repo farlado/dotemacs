dc1cd8545d4efe629a6866dc11c07eec5a249fca
Farlado <stultasergalo@gmail.com>
2019-12-03 19:44:29 -0500

    Fonts are now handled much smarter, no need for Makefile anymore

--- Makefile
+++
@@ -1,9 +0,0 @@
-# Makefile to choose font size
-
-lores:
-	git checkout init.el
-	git pull
-
-hires: lores
-	cat init.el | sed 's/100/220/' > init.el.new
-	mv init.el.new init.el
--- init.el
+++ init.el
@@ -14,13 +14,6 @@
 ;; most of the actual config is in here
 (org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
 
-;; Automatically created, don't touch these!
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 100 :width normal :family "Iosevka")))))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
