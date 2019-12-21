ab76c54dcee536e720912718afc20a0991b933cb
Farlado <stultasergalo@gmail.com>
2019-12-21 08:14:58 -0500

    Back to vterm uwu

--- init.el
+++ init.el
@@ -39,4 +39,4 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (system-packages avy-flycheck 2048-game chess epresent graphviz-dot-mode sudoku wttrin desktop-environment sudo-edit buffer-move emms haskell-mode company-jedi pretty-mode flycheck leuven-theme magit org-bullets avy smex swiper auto-package-update nov use-package dmenu exwm company diminish popup-kill-ring spaceline nov dashboard rainbow-delimiters hungry-delete smart-hungry-delete rainbow-mode ido-vertical-mode which-key))))
+    (vterm system-packages avy-flycheck 2048-game chess epresent graphviz-dot-mode sudoku wttrin desktop-environment sudo-edit buffer-move emms haskell-mode company-jedi pretty-mode flycheck leuven-theme magit org-bullets avy smex swiper auto-package-update nov use-package dmenu exwm company diminish popup-kill-ring spaceline nov dashboard rainbow-delimiters hungry-delete smart-hungry-delete rainbow-mode ido-vertical-mode which-key))))
