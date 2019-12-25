99dd880ed13ba9e9859ec6fb8e4914842d5365d6
Farlado <stultasergalo@gmail.com>
2019-12-25 16:29:34 -0500

    Clean up a few things, add a confirm for quitting emacs, etc

--- init.el
+++ init.el
@@ -27,12 +27,6 @@
 ;; Most of the actual config is in here
 (org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
 
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
