3b30397ac725762f3676a8de4429ff6423ffaf99
Farlado <stultasergalo@gmail.com>
2019-12-26 18:00:03 -0500

    May as well keep that around...

--- init.el
+++ init.el
@@ -32,3 +32,9 @@
  '(package-selected-packages
    (quote
     (vterm system-packages avy-flycheck 2048-game chess epresent graphviz-dot-mode sudoku wttrin desktop-environment sudo-edit buffer-move emms haskell-mode company-jedi pretty-mode flycheck leuven-theme magit org-bullets avy smex swiper auto-package-update nov use-package dmenu exwm company diminish popup-kill-ring spaceline nov dashboard rainbow-delimiters hungry-delete smart-hungry-delete rainbow-mode ido-vertical-mode which-key))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
