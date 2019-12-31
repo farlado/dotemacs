e733c31c9993aebb6c4b24bb609243c89129a54a
Farlado <stultasergalo@gmail.com>
2019-12-30 23:54:21 -0500

    Overhaul init.el layout, get rid of custom for good

--- init.el
+++ init.el
@@ -1,9 +1,12 @@
-;; Hide dumb stuff ASAP (kept in init.el for speed)
-(menu-bar-mode -1)
-(tooltip-mode -1)
-(tool-bar-mode -1)
-(scroll-bar-mode -1)
-(setq use-dialog-box nil)
+;;;;;;;;;;; Hide dumb stuff ASAP (kept in init.el for speed) ;;;;;;;;;;;
+(dolist (mode '(scroll-bar-mode menu-bar-mode
+                tool-bar-mode tooltip-mode))
+  (if (fboundp mode)
+      (funcall mode -1)))
+(setq use-dialog-box nil
+      use-file-dialog nil
+      indicate-empty-lines t)
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;;;;;;;;;;;;;;;;;;;;; These make Emacs load faster ;;;;;;;;;;;;;;;;;;;;;
 (setq gc-cons-threshold 402653184
@@ -37,25 +40,19 @@
   (package-initialize))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; Bootstrap use-package
+;;;;;;;;;;;;;;;;;;; Bootstrap async and use-package ;;;;;;;;;;;;;;;;;;;;
+(unless (package-installed-p 'async)
+  (package-refresh-contents)
+  (package-install 'async))
+(dired-async-mode 1)
+(async-bytecomp-package-mode 1)
+(setq async-bytecomp-allowed-packages '(all))
+
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;; The rest of the config is in an org-file
+;;;;;;;;;;;;;;; The rest of the config is in an org-file ;;;;;;;;;;;;;;;
 (org-babel-load-file (concat user-emacs-directory "config.org"))
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (markdown-mode vterm system-packages avy-flycheck 2048-game chess epresent graphviz-dot-mode sudoku wttrin desktop-environment sudo-edit buffer-move emms haskell-mode company-jedi pretty-mode flycheck leuven-theme magit org-bullets avy smex swiper auto-package-update nov use-package dmenu exwm company diminish popup-kill-ring spaceline nov dashboard rainbow-delimiters hungry-delete smart-hungry-delete rainbow-mode ido-vertical-mode which-key))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
